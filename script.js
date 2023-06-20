e3db19ff77bcc3cb3d4fe2097d9edf28fe250ffa
Nick J <njerschow@gmail.com>
2023-06-20 07:18:44 -0500

    added password length prompt and if condition

--- script.js
+++ script.js
@@ -7,19 +7,11 @@ var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharacters = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 
-
-// Needed to define generatePassword function
-function generatePassword() {
-// Prompt for criteria
-  // password length 8-128
-  // include lowercase, uppercase, numbers, special characters
-// validate the input. 
-// Genrate password based on criteria
-
-// Display password on page  
-
-  return "Generated password will go here!";
-}
+// Globally declared variables
+var passwordLength = " ";
+var confirmLowerCase;
+var confirmUpperCase;
+var confirmSpecialCharacters;
 
 // Write password to the #password input
 function writePassword() {
@@ -32,3 +24,30 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Needed to define generatePassword function
+function generatePassword() {
+  // Prompt for criteria
+    // password length 8-128
+    passwordLength = (prompt("How many characters would you like your password to contain? Choose between 9 and 128"));
+    console.log("Password length " + passwordLength);
+
+    if (passwordLength < 8 || passwordLength > 128) {
+    alert("Please choose between 8 and 128 characters");
+    
+  } else {
+    // confirm method allows for a prompt that creates a boolean value
+    confirmLowerCase = confirm("Do you want to include lower scase letters?");  
+  } 
+
+
+  
+    
+    // include lowercase, uppercase, numbers, special characters
+  // validate the input. 
+  // Genrate password based on criteria
+  
+  // Display password on page  
+  
+    return password;
+  }
