c64326e40e46cc32ab0243af116e61b7eab66022
Nick J <njerschow@gmail.com>
2023-06-20 06:32:11 -0500

    added arrays to js file

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Various Arrays for characters
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacters = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+
+
+// Needed to define generatePassword function
+function generatePassword() {
+// Prompt for criteria
+  // password length 8-128
+  // include lowercase, uppercase, numbers, special characters
+// validate the input. 
+// Genrate password based on criteria
+
+// Display password on page  
+
+  return "Generated password will go here!";
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
